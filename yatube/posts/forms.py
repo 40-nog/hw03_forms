25a3b9efaf201884c51a05e46a244672e0291ebc
Наталья Сорокина <natalya_40ina@mail.ru>
2022-07-28 18:52:45 +0300

    reviewed 1.0

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -6,10 +6,12 @@ class PostForm(forms.ModelForm):
     text = forms.CharField(
         widget=forms.Textarea,
         label='Напишите что-нибудь',
-        required=True
+        required=True,
+        help_text='Ваше сочинение',
     )
 
     class Meta:
         model = Post
         fields = ('group', 'text')
         labels = {'group': 'Выберите подходящую группу'}
+        help_texts = {'group': 'Группа'}
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,28 +5,28 @@ from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 
 
-def index(request):
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list, 10)
+def pagination(posts, request):
+    paginator = Paginator(posts, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    # Отдаем в словаре контекста
-    context = {
+    return {
         'page_obj': page_obj,
     }
+
+
+def index(request):
+    context = pagination(Post.objects.all(), request)
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    posts = group.posts.filter(group=group)
     context = {
         'group': group,
-        'page_obj': page_obj,
+        'posts': posts,
     }
+    context.update(pagination(group.posts.all(), request))
     return render(request, 'posts/group_list.html', context)
 
 
